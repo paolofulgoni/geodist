18fc4818382531c557b474e1a5dde95be148db87
Paolo Fulgoni <paolo.fulgoni@gmail.com>
2023-04-09 11:50:26 +0200

    Improve the distance method and add tests

--- Geodist.UnitTests/Domain/CosineLawDistanceCalculatorTests.cs
+++ Geodist.UnitTests/Domain/CosineLawDistanceCalculatorTests.cs
@@ -11,7 +11,12 @@ public class CosineLawDistanceCalculatorTests
     [InlineData(53.3409518, -6.2678073, 50.13397, 8.56566, 1079.4)]
     [InlineData(-34.3567014, 18.4745813, 47.7995985, -73.5394652, 12892.8)]
     [InlineData(47.7995985, -73.5394652, -34.3567014, 18.4745813,12892.8)]
-    public void Distance_WhenParametersAreValidAndDifferent_ReturnsCorrectDistance(
+    [InlineData(22.123, 131.123, -87.123, -171.123,12294.6)]
+    [InlineData(-87.123, -171.123,22.123, 131.123, 12294.6)]
+    [InlineData(0.0, -180.0,0.0, 180.0, 0)]
+    [InlineData(90.0, 12.0,90.0, -12.0, 0)]
+    [InlineData(-90.0, 12.0,-90.0, -12.0, 0)]
+    public void Distance_WhenDifferentPositions_ReturnsCorrectDistance(
         double pointALatitude,
         double pointALongitude,
         double pointBLatitude,
@@ -26,5 +31,47 @@ public class CosineLawDistanceCalculatorTests
 
         // assert
         distance.Should().BeApproximately(expectedDistance, expectedDistance * 0.0001);
+    }
+    
+    [Theory]
+    [InlineData(53.3409518, -6.2678073)]
+    [InlineData(-34.3567014, 18.4745813)]
+    public void Distance_WhenEqualPosition_ReturnsZero(
+        double pointLatitude,
+        double pointLongitude)
+    {
+        // arrange
+        var uut = new CosineLawDistanceCalculator();
+
+        // act
+        var distance = uut.Distance(pointLatitude, pointLongitude, pointLatitude, pointLongitude);
+
+        // assert
+        distance.Should().Be(0);
+    }
+    
+    
+    [Theory]
+    [InlineData(-90.1, 0, 0, 0)]
+    [InlineData(90.1, 0, 0, 0)]
+    [InlineData(0, -180.1, 0, 0)]
+    [InlineData(0, 180.1, 0, 0)]
+    [InlineData(0, 0, -90.1, 0)]
+    [InlineData(0, 0, 90.1, 0)]
+    [InlineData(0, 0, 0, -180.1)]
+    [InlineData(0, 0, 0, 180.1)]
+    public void Distance_WhenArgumentsAreOutOfRange_ThrowsArgumentException(
+        double pointALatitude,
+        double pointALongitude,
+        double pointBLatitude,
+        double pointBLongitude)
+    {
+        // arrange
+        var uut = new CosineLawDistanceCalculator();
+
+        // act
+        var act = () => uut.Distance(pointALatitude, pointALongitude, pointBLatitude, pointBLongitude);
+
+        act.Should().Throw<ArgumentException>();
     }
 }
--- Geodist/Domain/CosineLawDistanceCalculator.cs
+++ Geodist/Domain/CosineLawDistanceCalculator.cs
@@ -6,6 +6,17 @@ public class CosineLawDistanceCalculator : IGeographicalDistanceCalculator
     
     public double Distance(double pointALatitude, double pointALongitude, double pointBLatitude, double pointBLongitude)
     {
+        if (Math.Abs(pointALatitude) > 90.0) throw new ArgumentOutOfRangeException(nameof(pointALatitude));
+        if (Math.Abs(pointALongitude) > 180.0) throw new ArgumentOutOfRangeException(nameof(pointALongitude));
+        if (Math.Abs(pointBLatitude) > 90.0) throw new ArgumentOutOfRangeException(nameof(pointBLatitude));
+        if (Math.Abs(pointBLongitude) > 180.0) throw new ArgumentOutOfRangeException(nameof(pointBLongitude));
+        
+        // optimization in case point A and point B are equal
+        if (pointALatitude == pointBLatitude && pointALongitude == pointBLongitude)
+        {
+            return 0;
+        }
+        
         double a = DegToRad(90.0 - pointBLatitude);
         double b = DegToRad(90.0 - pointALatitude);
         double phi = DegToRad(pointALongitude - pointBLongitude);
