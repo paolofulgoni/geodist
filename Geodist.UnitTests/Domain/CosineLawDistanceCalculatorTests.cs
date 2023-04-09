ac85e1229ac6b5e326048e52fa6180753770de07
Paolo Fulgoni <paolo.fulgoni@gmail.com>
2023-04-09 11:26:31 +0200

    Implement the cosine law formula

--- Geodist.UnitTests/Domain/CosineLawDistanceCalculatorTests.cs
+++ Geodist.UnitTests/Domain/CosineLawDistanceCalculatorTests.cs
@@ -4,16 +4,27 @@ namespace Geodist.UnitTests.Domain;
 
 public class CosineLawDistanceCalculatorTests
 {
-    [Fact]
-    public void Distance_ReturnsValue_WhenParametersAreValid()
+    [Theory]
+    [InlineData(53.3409518, -6.2678073, 42.3514956, -71.0611723, 4809.4)]
+    [InlineData(42.3514956, -71.0611723, 53.3409518, -6.2678073, 4809.4)]
+    [InlineData(50.13397, 8.56566, 53.3409518, -6.2678073, 1079.4)]
+    [InlineData(53.3409518, -6.2678073, 50.13397, 8.56566, 1079.4)]
+    [InlineData(-34.3567014, 18.4745813, 47.7995985, -73.5394652, 12892.8)]
+    [InlineData(47.7995985, -73.5394652, -34.3567014, 18.4745813,12892.8)]
+    public void Distance_WhenParametersAreValidAndDifferent_ReturnsCorrectDistance(
+        double pointALatitude,
+        double pointALongitude,
+        double pointBLatitude,
+        double pointBLongitude,
+        double expectedDistance)
     {
         // arrange
         var uut = new CosineLawDistanceCalculator();
 
         // act
-        var distance = uut.Distance(0, 0, 0, 0);
+        var distance = uut.Distance(pointALatitude, pointALongitude, pointBLatitude, pointBLongitude);
 
         // assert
-        distance.Should().Be(5584);
+        distance.Should().BeApproximately(expectedDistance, expectedDistance * 0.0001);
     }
 }
--- Geodist/Domain/CosineLawDistanceCalculator.cs
+++ Geodist/Domain/CosineLawDistanceCalculator.cs
@@ -2,8 +2,18 @@ namespace Geodist.Domain;
 
 public class CosineLawDistanceCalculator : IGeographicalDistanceCalculator
 {
+    private const double _earthRadius = 6371;
+    
     public double Distance(double pointALatitude, double pointALongitude, double pointBLatitude, double pointBLongitude)
     {
-        return 5584;
+        double a = DegToRad(90.0 - pointBLatitude);
+        double b = DegToRad(90.0 - pointALatitude);
+        double phi = DegToRad(pointALongitude - pointBLongitude);
+
+        double p = Math.Acos(Math.Cos(a) * Math.Cos(b) + Math.Sin(a) * Math.Sin(b) * Math.Cos(phi));
+
+        return p * _earthRadius;
     }
+    
+    private static double DegToRad(double angleDeg) => angleDeg * Math.PI / 180.0;
 }
